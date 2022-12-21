d691065d01e7c87eafd45e3182bdde8ac9f16d12
clementhaon <clement.haon.13@gmail.com>
2022-12-21 16:28:33 +0100

    update env name

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ dotenv.config({ silent: true });
 const dt = Sequelize.DataTypes;
 
 const sequelize = new Sequelize(
-    `${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASS}`,
+    `${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`,
     {
         dialect: 'mysql',
         logging: false
