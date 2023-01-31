1d72ee073a9e598ec559bc39884b517bded91b4e
haon clement <clement.haon.13@gmail.com>
2023-01-31 15:28:37 +0100

    update user

--- models/users/controller.js
+++ models/users/controller.js
@@ -1,15 +1,25 @@
-import Router from 'express';
-const app = Router.Router();
+import express from "express";
+import jsonwebtoken from "jsonwebtoken";
+import models from "../index";
+const app = express.Router();
 
 
 const testT = (req, res) => {
 
+    try {
 
-    res.status(200).send({success: true});
+        return res.status(200).json('lee');
+
+    } catch (err) {
+        console.log(err)
+
+        return res.status(500).send({success:false, message: err?.message ? err?.message : ""});
+
+    }
 
 
 }
 
 app.get('test', testT);
 
-export default app
+export default app;
